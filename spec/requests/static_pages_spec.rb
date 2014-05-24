b339263f7b5f0bb0fdfe198f06e4883e8d270a6b
Vitalik <ananichev.v.v@gmail.com>
2014-05-24 14:00:51 +0300

    Finish the contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,18 @@ describe "StaticPages" do
   	
   end
   
+  describe "Contact pages" do
+  	
+  	it "should have the content 'Contact'" do
+  	  visit '/static_pages/contact'
+  	  expect(page).to have_content('Contact')
+  	end
+  	
+  	it "should have the title 'Contact'" do
+  	  visit "/static_pages/contact"
+  	  expect(page).to have_title('Contact')
+  	end
+  	
+  end
+  
 end
